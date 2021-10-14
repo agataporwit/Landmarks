8ffd23302ca7422883ab3a6990d90d2644012810
Agata <agata@porwit.net>
2021-10-14 16:08:41 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Agata Porwit on 10/14/21.
+//
+
+import SwiftUI
+//mainattribute identifies the app’s entry point.
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,35 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Agata Porwit on 10/14/21.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var Image: Image {
+        SwiftUI.Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude,
+                               longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+        
+    }
+}
