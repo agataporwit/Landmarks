ac5161fdaaec0debf7ab8c5dd079b32ebce3acb4
Agata <agata@porwit.net>
2021-10-18 17:36:05 -0700

    Composing Complex Interfaces (tutorial by apple continue)

--- Landmarks/Model/Hike.swift
+++ Landmarks/Model/Hike.swift
@@ -13,19 +13,19 @@ struct Hike: Codable, Hashable, Identifiable {
     var distance: Double
     var difficulty: Int
     var observations: [Observation]
-    
+
     static var formatter = LengthFormatter()
-    
+
     var distanceText: String {
-            return Hike.formatter
-                .string(fromValue: distance, unit: .kilometer)
-        }
+        return Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
 
-        struct Observation: Codable, Hashable {
-            var distanceFromStart: Double
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
 
-            var elevation: Range<Double>
-            var pace: Range<Double>
-            var heartRate: Range<Double>
-        }
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
 }
